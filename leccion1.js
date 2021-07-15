54be651ab603b1369f9463fec7674573021664f3
ernestoroca <ernesto.roca@gmail.com>
2021-07-14 21:38:43 -0400

    Update leccion1.js

--- leccion1.js
+++ leccion1.js
@@ -1,1 +1,99 @@
-var preguntas = ["a board","a door","a duck","a window","a book","a desk","an apple","a pencil","an eraser","a sharpener","it's a board","it's a sharpener","it's a pencil"];
+var unidad1 = [
+  "a board",
+  "a door",
+  "a duck",
+  "a window",
+  "a book",
+  "a desk",
+  "an apple",
+  "a pencil",
+  "an eraser",
+  "a sharpener",
+  
+  "it is a board",
+  "it is a sharpener",
+  "it is a pencil",
+  "it is a book",
+  "it is an eraser",
+  "it is a window",
+  
+  "it is a door",
+  "it is a desk",
+  "it is an apple",
+  "it is a duck",
+  
+  "what is this?|it is a sharpener",
+  "what is this?|it is a pencil",
+  "what is this?|it is a board",
+  "what is this?|it is a book",
+  
+  "what is this?|it is an eraser",
+  "what is this?|it is a desk",
+  "what is this?|it is an apple",
+  "what is this?|it is a door"
+];
+
+var unidad2 = [
+  "a teacher",
+  "a clock",
+  "a wall",
+  "a girl",
+  "a schoolbag",
+  "a boy",
+  "a ruler",
+  "a crayon",
+  "a trash can",
+  "a box",
+  
+  "is this a schoolbag?|no, it is not",
+  "is this a clock?|yes, it is",
+  "is this a pencil?|no, it is not",
+  "is this an eraser?|yes, it is",
+  "is this a sharpener?|yes, it is"
+  "is this a girl?|no, it is not",
+  
+  "is this a girl?|no, it is not",
+  "is this a box?|yes, it is",
+  "is this a wall?|no, it is not",
+  "is this a ruler?|yes, it is",
+  "is this a window?|yes, it is",
+  "is this a door?|no, it is not",
+  
+  "it's a pencil",
+  "it's a wall",
+  "it's a ruler",
+  "it's a clock",
+  "it's an eraser",
+  "it's a pencil",
+  "it's an apple",
+  
+  "what is this?|it is a trash can",
+  "what is this?|it is a window",
+  "what is this?|it is a wall",
+  "what is this?|it is a pencil",
+  "what is this?|it is an eraser",
+  "what is this?|it is a schoolbag",
+  
+  "is this a girl?|yes, it is",
+  "is this a clock?not, it isn't",
+  "is this a crayon?|yes, it is",
+  "is this an eraser?|no, it isn't",
+  "is this a teacher?|yes, it is",
+  "is this a box?|no, it isn't",
+  
+  
+  
+  
+  
+];
+  
+  
+  
+  
+
+
+
+
+
+
+];
