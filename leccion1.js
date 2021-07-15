9388639716f847a82e4c6f091380f13bcc68af8e
ernestoroca <ernesto.roca@gmail.com>
2021-07-14 20:31:43 -0400

    Update leccion1.js

--- leccion1.js
+++ leccion1.js
@@ -1,1 +1,1 @@
-var preguntas1 = ["a board","a door","a duck","a window","a book","a desk","an apple","a pencil","an eraser","a sharpener","it's a board","it's a sharpener","it's a pencil"];
+var preguntas = ["a board","a door","a duck","a window","a book","a desk","an apple","a pencil","an eraser","a sharpener","it's a board","it's a sharpener","it's a pencil"];
